a24192071c8f1a244d9b9260aa1159a17a2c77ab
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2024-10-31 13:11:04 +0300

    work

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,6 +2,31 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
-window.addEventListener("load", function() {
-    document.body.classList.remove("load");
-  });
+window.addEventListener("load", function () {
+  document.body.classList.remove("load");
+});
+
+
+
+
+
+
+// Добавление и удаления класса по клику
+
+
+let block = document.getElementsByClassName('product-item__favorite');
+for (let i = 0; i < block.length; i++) {
+  block[i].onclick = function () {
+    this.classList.toggle('active');
+  }
+}
+
+// Добавление и удаления класса по клику
+// Выбор блока по ID
+
+// let block = document.getElementById("favorite-id");
+
+//   block.onclick = function () {
+//     console.log('Fci');
+//     this.classList.toggle('active');
+//   }
