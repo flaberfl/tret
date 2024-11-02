92459142b0c7fec8c91083b204ee168d327ee782
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2024-11-02 22:45:06 +0300

    home

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -148,17 +148,91 @@ function initSliders() {
 				0: {
 					slidesPerView: 2,
 					spaceBetween: 20,
-					autoHeight: true,
+					// autoHeight: true,
 				},
 				360: {
 					slidesPerView: 2,
-					spaceBetween: 20,
-					autoHeight: true,
+					spaceBetween: 18,
+					// autoHeight: true,
 				},
 				640: {
 					slidesPerView: 2,
 					spaceBetween: 20,
-					autoHeight: true,
+					// autoHeight: true,
+				},
+				768: {
+					slidesPerView: 2,
+					spaceBetween: 20,
+				},
+				992: {
+					slidesPerView: 3,
+					spaceBetween: 28,
+				},
+				1268: {
+					slidesPerView: 4,
+					spaceBetween: 28,
+				},
+			},
+
+			// События
+			on: {
+
+			}
+		});
+	}
+
+	if (document.querySelector('.filter-category__slider')) { //Указываем класс нужного слайдера
+		//Создаем слайдер
+		new Swiper('.filter-category__slider', { //Указываем класс нужного слайдера
+			//Подключаем модули слайдера
+			//для конкретного случая
+			modules: [Navigation],
+			observer: true,
+			observeParents: true,
+			slidesPerView: 4,
+			spaceBetween: 28,
+			// effect: "fade",
+			// autoHeight: true,
+			speed: 800,
+
+			//touchRatio: 0,
+			//simulateTouch: false,
+			loop: true,
+			//preloadImages: false,
+			lazy: true,
+
+			/*
+			// Эффекты
+			effect: 'fade',
+			autoplay: {
+				delay: 3000,
+				disableOnInteraction: false,
+			},
+			*/
+
+
+			// Кнопки "влево/вправо"
+			navigation: {
+				prevEl: '.filter-category__slider-btn.swiper-button-prev',
+				nextEl: '.filter-category__slider-btn.swiper-button-next',
+			},
+
+			// Брейкпоинты
+			breakpoints: {
+				0: {
+					slidesPerView: 2,
+					spaceBetween: 20,
+					// autoHeight: true,
+				},
+				360: {
+					slidesPerView: 2,
+					spaceBetween: 20,
+					// autoHeight: true,
+				},
+				640: {
+					slidesPerView: 2,
+					spaceBetween: 20,
+					// autoHeight: true,
 				},
 				768: {
 					slidesPerView: 2,
