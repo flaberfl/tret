2c357d56864c6c314edfd7310d2f35222da34580
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2024-11-04 20:18:18 +0300

    Update scroll.js

--- src/js/files/scroll/scroll.js
+++ src/js/files/scroll/scroll.js
@@ -89,13 +89,13 @@ export function headerScroll() {
 	addWindowScrollEvent = true;
 	const header = document.querySelector('header.header');
 	const headerShow = header.hasAttribute('data-scroll-show');
-	const headerShowTimer = header.dataset.scrollShow ? header.dataset.scrollShow : 500;
+	const headerShowTimer = header.dataset.scrollShow ? header.dataset.scrollShow : 0;
 	const startPoint = header.dataset.scroll ? header.dataset.scroll : 1;
 	let scrollDirection = 0;
-	let timer;
+	// let timer;
 	document.addEventListener("windowScroll", function (e) {
 		const scrollTop = window.scrollY;
-		clearTimeout(timer);
+		// clearTimeout(timer);
 		if (scrollTop >= startPoint) {
 			!header.classList.contains('_header-scroll') ? header.classList.add('_header-scroll') : null;
 			if (headerShow) {
@@ -106,9 +106,9 @@ export function headerScroll() {
 					// upscroll code
 					!header.classList.contains('_header-show') ? header.classList.add('_header-show') : null;
 				}
-				timer = setTimeout(() => {
-					!header.classList.contains('_header-show') ? header.classList.add('_header-show') : null;
-				}, headerShowTimer);
+				// timer = setTimeout(() => {
+				// 	!header.classList.contains('_header-show') ? header.classList.add('_header-show') : null;
+				// }, headerShowTimer);
 			}
 		} else {
 			header.classList.contains('_header-scroll') ? header.classList.remove('_header-scroll') : null;
