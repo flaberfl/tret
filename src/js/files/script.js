5b46862a9ff470082fca1a21b3ea98b0e70ad047
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2024-11-21 21:34:50 +0300

    home

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -83,6 +83,13 @@ for (let i = 0; i < addToBasket.length; i++) {
   }
 }
 
+let addSocial = document.getElementsByClassName('contact-social__button');
+for (let i = 0; i < addSocial.length; i++) {
+  addSocial[i].onclick = function () {
+    this.classList.toggle('active');
+  }
+}
+
 let deliveryMetod = document.getElementsByClassName('shipping-checkout__item-list');
 for (let i = 0; i < deliveryMetod.length; i++) {
   deliveryMetod[i].onclick = function () {
